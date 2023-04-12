40a0a82c5dff1a59f055144746d9b51e4dfada0f
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-04-11 22:46:04 -0300

    commit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -7,22 +7,9 @@ export const addFav = (character) => {
     }
 }
 
-export const removeFav = (character) => {
+export const removeFav = (id) => {
     return {
-        type: "REMOVE_FAV", payload: {id}
+        type: "REMOVE_FAV", payload: id
     }
 }
 
-
-
-// Construye dos actions-creators:
-
-// addFav: esta función recibe un personaje por parámetro. Deberás retornar una action con el type igual a "ADD_FAV", y el payload igual a ese 
-// personaje.
-
-// removeFav: esta función recibe un id por parámetro. Deberás retornar una action con el type igual a "REMOVE_FAV", y el payload igual a ese id.
-
-// Exporta ambas funciones.
-
-// [NOTA]: no olvides que el nombre que asignes en la propiedad "TYPE" de tu acción, debe coincidir exactamente con el nombre de los casos que 
-// hayas asignado en tu reducer.
---
+++ src/redux/reducer.js
@@ -0,0 +1,49 @@
+
+const initialState = {
+    myFavorites: [],
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_FAV": {
+            return {
+                ...state,
+                myFavorites: [...state.myFavorites, action.payload.character]
+            };
+        }
+        case "REMOVE_FAV": {
+            const idRemove = action.payload.id
+            const updateFavorites = state.myFavorites.filter(
+                (char) => char.id !== idRemove
+            );
+            return {
+                ...state,
+                myFavorites: updateFavorites
+            };
+        }
+
+        default:
+            return { ...state }
+    }
+}
+
+
+export default rootReducer;
+
+
+// Dirígete a tu archivo reducer.js. Allí deberás:
+
+// Crear un initialState con una propiedad llamada "myFavorites". Esta propiedad será un arreglo vacío.
+
+// Luego deberás crear tu reducer. Recuerda que este recibe dos parámetros y que dentro de él hay un switch.
+
+// [NOTA]: ten en cuenta el modo en el que lo exportas, y cómo lo importas dentro de tu store.
+
+// Crea un primer caso llamado "ADD_FAV" en el que puedas agregar a un personaje que recibes por payload a tu estado "myFavorites".
+
+// Crea otro caso llamado "REMOVE_FAV" en el que puedas eliminar a un personaje de tu estado "myFavorites" a partir de un id que recibas por
+// payload.
+
+// [NOTA]: ten en cuenta que el id que recibes por payload es un string, y el id de los personajes es un número.
+
+// No te olvides de tu caso default.
