bb9c64337b9b1e9a287687350b4f4b84c5ce612a
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-04-12 12:10:51 -0300

    commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import styles from './Card.module.css'
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addFav, removeFav } from '../../redux/actions';
+import { connect } from 'react-redux';
 
-function Card({ id, name, species, gender, image, onClose }) {
+function Card({ id, name, species, gender, image, onClose, addFav, removeFav }) {
+   const [isFav, setIsFav] = useState(false);
+   const dispatch = useDispatch();
+
+   const handleFavorite = () => {
+      if(isFav) {
+         return setIsFav(false)
+      }
+   }
+
    return (
       <div className={styles.card}>
          <button className={styles.closeButton} onClick={() => onClose(id)}>
@@ -11,7 +24,7 @@ function Card({ id, name, species, gender, image, onClose }) {
 
          <div className={styles.body}>
             <Link to={`/detail/${id}`}>
-            <h2 className={styles.name}>Name: {name}</h2>
+               <h2 className={styles.name}>Name: {name}</h2>
             </Link>
             <h2 className={styles.species}>Species: {species}</h2>
             <h2 className={styles.gender}>Gender: {gender}</h2>
@@ -22,4 +35,15 @@ function Card({ id, name, species, gender, image, onClose }) {
    );
 }
 
-export default Card;
+const mapDispatchToProps = (dispatch) => {
+   return {
+      addFav: (character) => {
+         dispatch(addFav(character))
+      },
+      removeFav: (id) => {
+         dispatch(removeFav(id))
+      }
+   }
+}
+
+export default connect(null, mapDispatchToProps)(Card);
