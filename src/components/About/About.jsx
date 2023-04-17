49b289f5cd29a9d41822f36b3d3211143c473323
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-04-17 10:17:10 -0300

    commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import styles from './About.module.css'
 
 class About extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props)
     }
 
-    render () {
+    render() {
         return (
             <>
                 <h1 className={styles.h1}> Hola!👋 <br /> Soy <span>Federico Risetti</span> y esta es un pagina web para buscar a los 826 personajes de Rick And Morty.
-                La misma utiliza una API que nos trae la información de la serie. Todo fue realizado mediantes React, Redux y los estilos fueron aplicados
-                con CSS. ¡Espero que te guste!</h1>
+                    La misma utiliza una <span>API</span> con la info de la serie. Todo fue realizado mediante <span>React</span>, <span>Redux</span> y los estilos fueron aplicados
+                    con <span>LESS</span> y <span>CSS</span>. <br /> ¡Espero que te guste!</h1>
             </>
         )
     }
